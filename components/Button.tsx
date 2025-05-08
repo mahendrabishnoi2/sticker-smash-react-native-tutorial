cd6db7c9e63160531baf5af54dff934bc3aa7f11
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2025-05-08 13:14:02 +0530

    - add a new prop in button component to specify a theme (primary for now) - in case theme is primary, button will have an icon, border while background and few more overridden styles compared to a button without theme - use primary theme for "Choose a photo" button on home page

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,37 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type Props = {
   label: string;
+  theme?: "primary";
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You preseed a button.")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button}>
@@ -30,6 +57,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: "#fff",
